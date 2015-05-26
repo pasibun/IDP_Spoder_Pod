ebb34b1e8379663b22b73a0a7757918f677e27a0
Nigel2x4 <hwesterhof@live.nl>
2015-05-26 12:12:56 +0200

    Base for SensorService service.

---
+++ hexapod/src/main/java/org/nhl/spoderpod/hexapod/components/C_SensorReader.java
@@ -0,0 +1,35 @@
+package org.nhl.spoderpod.hexapod.components;
+
+import org.nhl.spoderpod.hexapod.core.MessageBus;
+import org.nhl.spoderpod.hexapod.interfaces.IMessage;
+
+public class C_SensorReader extends BaseComponent {
+
+	public C_SensorReader(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void init(MessageBus messageBus) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void close(MessageBus messageBus) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected boolean composeMessage(MessageBus messageBus) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected void receiveMessage(MessageBus messageBus, IMessage message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

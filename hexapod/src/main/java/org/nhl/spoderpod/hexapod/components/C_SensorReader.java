107ca0bdb8e17c303868540bd5dff0992df10e0c
Nigel2x4 <hwesterhof@live.nl>
2015-05-27 13:40:05 +0200

    Gezellig aan de protocol werken! ^^

--- hexapod/src/main/java/org/nhl/spoderpod/hexapod/components/C_SensorReader.java
+++ hexapod/src/main/java/org/nhl/spoderpod/hexapod/components/C_SensorReader.java
@@ -1,17 +1,21 @@
 package org.nhl.spoderpod.hexapod.components;
 
+import org.nhl.spoderpod.hexapod.core.ComponentRef;
 import org.nhl.spoderpod.hexapod.core.MessageBus;
 import org.nhl.spoderpod.hexapod.interfaces.I_Message;
+import org.nhl.spoderpod.hexapod.utils.U_SensorReadWrite;
 
 public class C_SensorReader extends BaseComponent {
 
+	private U_SensorReadWrite u_SensorRW;
+	
 	public C_SensorReader(String name) {
 		super(name);
 		// TODO Auto-generated constructor stub
 	}
 
 	public void init(MessageBus messageBus) {
-		// TODO Auto-generated method stub
+		this.u_SensorRW = new U_SensorReadWrite();
 		
 	}
 
@@ -22,8 +26,8 @@ public class C_SensorReader extends BaseComponent {
 
 	@Override
 	protected boolean composeMessage(MessageBus messageBus) {
-		// TODO Auto-generated method stub
-		return false;
+		new ComponentRef("DataFormat").tell(messageBus, getSelf(), "afstand");
+		return true;
 	}
 
 	@Override

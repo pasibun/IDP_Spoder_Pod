0a0e9dbbfa7e03fadb302d75d538414a08328fec
Nigel2x4 <hwesterhof@live.nl>
2015-06-02 13:25:42 +0200

    fak

--- hexapod/src/main/java/org/nhl/spoderpod/hexapod/libraries/L_FileActions.java
+++ hexapod/src/main/java/org/nhl/spoderpod/hexapod/libraries/L_FileActions.java
@@ -1,16 +1,20 @@
 package org.nhl.spoderpod.hexapod.libraries;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class L_FileActions {
 
-	public static byte[] read(){
-		Path path = Paths.get("dev/ttyAMA0");
+	public static List<Byte> read(){
+		Path path = Paths.get("test.txt");
 		byte[] data = null;
 		try {
 			data = Files.readAllBytes(path);
@@ -18,25 +22,38 @@ public class L_FileActions {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		return data;
+		return turnList(data);
 	}
 	
-	public static boolean write(List<Byte> message){
-		
-		try {
-			FileWriter fw = new FileWriter("dev/ttyAMA0"); 
-			String s = "";
-			for(byte b:message){
-				s += b;
-			}
-			fw.write(s);
-		} catch (IOException e) {
-			return false;
+
+	private static List<Byte> turnList(byte[] data){
+		List<Byte> awesomeSauce = new ArrayList<Byte>();
+		for(int i = 0; i < data.length; i++){
+			awesomeSauce.add(data[i]);
 		}
-		
-		return true;
+		return awesomeSauce;
+	}
+	
+	private static byte[] turnArray(List<Byte> winList){
+		byte[] awesomeSauce = new byte[winList.size()];
+		for(int i = 0; i < winList.size(); i++){
+			awesomeSauce[i] = winList.get(i);
+		}
+		return awesomeSauce;
 		
 	}
 	
+	public static void write(List<Byte> message){
+		BufferedOutputStream bs = null;
+		try {
+			//"/dev/ttyAMA0"
+		    FileOutputStream fs = new FileOutputStream(new File("test.txt"));
+		    bs = new BufferedOutputStream(fs);
+		    bs.write(turnArray(message));
+		    bs.close();
+		    bs = null;
+		} catch (Exception e) {
+		    e.printStackTrace();
+		}
+	}
 }

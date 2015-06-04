1ab28987134e76e299064ebc9b90be438c2532e9
pasibun <matthijs_hovestad@hotmail.com>
2015-06-04 11:56:32 +0200

    read adruino file sensor service

--- hexapod/src/main/java/org/nhl/spoderpod/hexapod/components/C_SensorReader.java
+++ hexapod/src/main/java/org/nhl/spoderpod/hexapod/components/C_SensorReader.java
@@ -1,5 +1,10 @@
 package org.nhl.spoderpod.hexapod.components;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 import org.nhl.spoderpod.hexapod.core.ComponentRef;
 import org.nhl.spoderpod.hexapod.core.MessageBus;
 import org.nhl.spoderpod.hexapod.interfaces.I_Message;
@@ -8,7 +13,7 @@ import org.nhl.spoderpod.hexapod.utils.U_SensorReadWrite;
 public class C_SensorReader extends BaseComponent {
 
 	private U_SensorReadWrite u_SensorRW;
-	
+
 	public C_SensorReader(String name) {
 		super(name);
 		// TODO Auto-generated constructor stub
@@ -16,12 +21,12 @@ public class C_SensorReader extends BaseComponent {
 
 	public void init(MessageBus messageBus) {
 		this.u_SensorRW = new U_SensorReadWrite();
-		
+
 	}
 
 	public void close(MessageBus messageBus) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -32,10 +37,23 @@ public class C_SensorReader extends BaseComponent {
 
 	@Override
 	protected void receiveMessage(MessageBus messageBus, I_Message message) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	
+		// TODO Auto-generated method stubw
 
+	}
+
+	/**
+	 * Read the servo file from the adruino
+	 * 
+	 * TODO: Wat moet hij doen verder met de data die hij uitleest?
+	 * 
+	 * @throws IOException
+	 */
+	private void read() throws IOException {
+		Path path = Paths.get("/dev/ttyAMA0");
+		try (Scanner scanner = new Scanner(path)) {
+			while (scanner.hasNextLine()) {
+				// doe dingen
+			}
+		}
+	}
 }

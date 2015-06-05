50af8b131408e387148b71add1c6f0f2300018ec
pasibun <matthijs_hovestad@hotmail.com>
2015-06-05 10:25:08 +0200

    kleine aanpassing logger to file

--- hexapod/src/main/java/org/nhl/spoderpod/hexapod/components/C_SensorReader.java
+++ hexapod/src/main/java/org/nhl/spoderpod/hexapod/components/C_SensorReader.java
@@ -28,42 +28,45 @@ public class C_SensorReader extends BaseComponent {
 	}
 
 	/***
-	 * ComposeMessage for the this object makes sure that incoming datapackages are dissected based on the type.
-	 * And are send to the correct service. The actual communication towards the correct service are a TODO!!
+	 * ComposeMessage for the this object makes sure that incoming datapackages
+	 * are dissected based on the type. And are send to the correct service. The
+	 * actual communication towards the correct service are a TODO!!
 	 * 
-	 * Data formatting towards the component making sure of the inter-service communication are also still being developed. 
+	 * Data formatting towards the component making sure of the inter-service
+	 * communication are also still being developed.
 	 */
 	@Override
-//	0; Wordt gebruikt voor Debugging
-//	1; Wordt gebruikt voor Movements
-//	2; Wordt gebruikt voor Servos
-//	3; Wordt gebruikt voor Sensors
-//	4; Wordt gebruikt voor Gyro
-//	5; Wordt gebruikt voor joystick
-//	6; Wordt gebruikt voor buttons
-//	7; Wordt gebruikt voor touchpad
+	// 0; Wordt gebruikt voor Debugging
+	// 1; Wordt gebruikt voor Movements
+	// 2; Wordt gebruikt voor Servos
+	// 3; Wordt gebruikt voor Sensors
+	// 4; Wordt gebruikt voor Gyro
+	// 5; Wordt gebruikt voor joystick
+	// 6; Wordt gebruikt voor buttons
+	// 7; Wordt gebruikt voor touchpad
 	protected boolean composeMessage(MessageBus messageBus) {
 		String strReceiver = "Logger";
 		int intData = 0;
 		int intId = 0;
 		L_Decoder.recieveMsg((L_FileActions.read()));
-		for (DataPackage dp : L_Decoder.getData()){
+		for (DataPackage dp : L_Decoder.getData()) {
 			int intType = dp.get_byteType();
 			intData = dp.get_shortData();
 			intId = dp.get_byteId();
-			switch(intType){
-				case 2: 
-					strReceiver = "S_Logger";
-				case 3:
-				case 4:
-					strReceiver = "S_AI";
-				case 5:
-				case 6:
-				case 7:
-					strReceiver = "S_Human";
+			switch (intType) {
+			case 2:
+				strReceiver = "S_Logger";
+			case 3:
+			case 4:
+				strReceiver = "S_AI";
+			case 5:
+			case 6:
+			case 7:
+				strReceiver = "S_Human";
 			}
 		}
-		new ComponentRef(strReceiver).tell(messageBus, getSelf(), String.format("[%s %s]", intData, intId));
+		new ComponentRef(strReceiver).tell(messageBus, getSelf(),
+				String.format("[%s %s]", intData, intId));
 		return true;
 	}
 

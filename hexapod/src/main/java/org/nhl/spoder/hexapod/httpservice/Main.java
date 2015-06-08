dc7b984289e0ac559bc6189f39acae0ab3e9f935
Nigel2x4 <hwesterhof@live.nl>
2015-06-08 15:34:43 +0200

    Zie vorige commit.

--- hexapod/src/main/java/org/nhl/spoder/hexapod/httpservice/Main.java
+++ hexapod/src/main/java/org/nhl/spoder/hexapod/httpservice/Main.java
@@ -1,7 +1,6 @@
 package org.nhl.spoder.hexapod.httpservice;
 
 import org.nhl.spoderpod.hexapod.components.C_HTTPAppSocket;
-import org.nhl.spoderpod.hexapod.components.C_HTTPFormat;
 import org.nhl.spoderpod.hexapod.components.C_RouterClient;
 import org.nhl.spoderpod.hexapod.core.Service;
 import org.nhl.spoderpod.hexapod.interfaces.I_Component;

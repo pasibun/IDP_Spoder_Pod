b14ca77d6e89cc0a055ad0d1ba7f41a897971f4c
pasibun <matthijs_hovestad@hotmail.com>
2015-06-11 16:57:08 +0200

    Even wat informatie voor de gene die er nog mee bezig gaat.

--- hexapod/src/main/java/org/nhl/spoderpod/hexapod/components/C_AICalculate.java
+++ hexapod/src/main/java/org/nhl/spoderpod/hexapod/components/C_AICalculate.java
@@ -18,29 +18,56 @@ public class C_AICalculate extends BaseComponent {
 	 * Method calculates the direction the spider has to walk to when it gets a
 	 * message.
 	 * 
+	 * TODO: Case 5 moet er wat gebeuren met de data die binnenkomt, dit is van
+	 * de joystick. Dit wordt verstuurd door lukas in een short met x en y
+	 * waarden. Vraag even aan fre hoe het er precies uitziet en aan de hand
+	 * daarvan moet hij de juist gegevens doorsturen naar de movement server.
+	 * (dit mag gewoon vooruit, links, rechts, stop ofzo zijn.
+	 * 
+	 * TODO: Case 6 zijn de buttons, als het goed is 1 ID een sla beweging om de
+	 * ballonnen kapot te maken. (vraag aan yannick hoe de data in de movement
+	 * server binnen moet komt).
+	 * 
+	 * TODO: Case 3 is geloof ik prima, dit is de afstand sensor. Volgens mij is
+	 * dit prima verder, vraag nog even aan yannink/fre? hoe hij de waarden moet
+	 * krijgen in de movement server.
+	 * 
+	 * Info: Laatste puntje ik heb MessageInt aangemaakt zodat de data een int
+	 * is. Ik weet nu even niet of de data vanuit de C_SensorReader een andere
+	 * format moet hebben. Misschien moeten hier de "[" en "]" er uit. Overleg
+	 * dit even met Lukas.
+	 * 
 	 * @param message
 	 * @return string based answer of the direction the spider has to walk to.
 	 */
-	private int calcDirection(MessageInt message) {
+	private String calcDirection(MessageInt message) {
 		if (message instanceof MessageInt) {
 			MessageInt m = (MessageInt) message;
 			switch (shit[0]) {
 			case 3:
-				return shit[2];
+				/**
+				 * als de waarde van de sensor server kleiner is dan 3 dan moet
+				 * moet hij stoppen, de afstand sensor kan niet onder de 3 cm
+				 * meeten.
+				 */
+				if (shit[2] > 3)
+					return "Vooruit";
+				else
+					return "Stop";
 			case 5:
-				
+
 				break;
 			case 6:
 				if (shit[1] == 0) {
 					// doe dingen met id 0
-				} else {
+				} else if (shit[1] == 1) {
 					// doe dingen met id 1
 				}
 				break;
 			}
 		}
 
-		return 2;
+		return "";
 	}
 
 	/**
@@ -75,14 +102,12 @@ public class C_AICalculate extends BaseComponent {
 		return true;
 	}
 
-	// intType, intId, intData
 	@Override
 	protected void receiveMessage(MessageBus messageBus, I_Message message) {
 		if (message instanceof MessageInt) {
 			MessageInt m = (MessageInt) message;
 			shit = m.getData();
-			System.out.println(m.getData());
-			//calcDirection(m);
+			calcDirection(m);
 		}
 	}
 
